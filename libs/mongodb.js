9068fd501c7a5aafd3b75739d890972adf96a4ad
Amit Yadav <codewithamit1544@gmail.com>
2024-04-22 23:08:25 +0530

    commented

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const connectMongoDb = async() => {
-    try{
-        await mongoose.connect(process.env.MongoDB_URI);
-        console.log("coonnnect successfully")
-    } catch(error){
-        console.log(error);
-    }
-};
-export default connectMongoDb;
+// const connectMongoDb = async() => {
+//     try{
+//         await mongoose.connect(process.env.MongoDB_URI);
+//         console.log("coonnnect successfully")
+//     } catch(error){
+//         console.log(error);
+//     }
+// };
+// export default connectMongoDb;
